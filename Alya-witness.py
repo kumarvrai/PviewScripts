7a27a98354becad71435129abb55694f35324714
vishal kumar <16vk9@queensu.ca>
2023-06-14 00:18:34 -0400

    general changes

--- Alya-witness.py
+++ Alya-witness.py
@@ -144,11 +144,12 @@ airfoil 	= sys.argv[2]
 side 		= sys.argv[3];
 mode 		= sys.argv[4]
 
-#indP = [0,1,3,5]
-#indP = [0,1,2,3,4]
-#indP = [4]
+#indP = [2,3,4]
+indP = [0,1,2,3,4]
+#indP = [0,2,6]
 #indP = [8,10,12,14]
-#indP = [288]
+#indP = [12,23,28]
+#indP = [28,29,30]
 #indP = [4,5,6,7]
 #indP = [0,1,2,3,4,5,6]
 #indP = [11,12,13]
@@ -156,7 +157,10 @@ mode 		= sys.argv[4]
 #indP = [5,6,7,8,9]
 #indP = [2,3,4,5,6,7,8,9,10,11]
 #indP = (np.arange(23,30)-1)
-#indP = [22,23,24]
+#indP = (np.arange(1,7)-1)
+#indP = np.array([1,3,5,7,8,10])-1
+#indP = np.array([2,6,10])-1
+#indP = np.array([2,4,6,8])-1
 #indP = [581,584] #SLE-5 (0.6,0.9) to compare 
 #indP = [288,290,291] #TBL-5 (0.6,0.9) to compare 
 #indP = [480,481,482,483] # RLE2 - TE-SP (0.2,0.3,0.4,0.5) location
@@ -181,15 +185,15 @@ mode 		= sys.argv[4]
 # RLE1-5 F @ z/c=0.04; S @ z/c=0.021
 #indP = [232,234,235,120,122,123] #RLE1-5 [F,F,F,S,S,S] 
 
-x_loc = [0.9, 0.95]
+x_loc = [1.2, 1.5]
 
 # LOAD AIRFOIL SPECIFIC FILES
 print('--|| ALYA INITIALIZING')
 plt.close("all")
-lw = 2;
-SMALL_SIZE = 20
-MEDIUM_SIZE = 24
-BIGGER_SIZE = 28
+lw = 1.2;
+SMALL_SIZE = 12
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 20
 plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
 plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
 plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
@@ -249,20 +253,22 @@ if("SHOWPTS" in mode):
   plt.plot(coordAirU[:,0],coordAirU[:,1],'k',linewidth=2)
   plt.plot(coordAirL[:,0],coordAirL[:,1],'k',linewidth=2)
   leg = []
-  for n in range(nz):
+  #for n in range(nz):
+   #lab = r'$P_{'+str(n+1)+'}$';
+  for n in range(len(indP)):
+   pt = indP[n]
    lab = r'$P_{'+str(n+1)+'}$';
-   
    #if(n<11):
    # sgnX = -1
    # sgnY = 1 
    #else:
    sgnX = 1
-   sgnY = -1 
+   sgnY = 1 
    #if n in [0,1,2,11,12,13]:
    # sgnY = 0
    leg.append(lab)
-   plt.plot(witPoints[n,0], witPoints[n,1],'o',linewidth=0.5)
-   plt.text(witPoints[n,0]+(sgnX)*0.025, witPoints[n,1]+(sgnY)*0.025, lab, fontsize=MEDIUM_SIZE)
+   plt.plot(witPoints[pt,0], witPoints[pt,1],'ko',linewidth=0.5)
+   plt.text(witPoints[pt,0]+(sgnX)*0.025, witPoints[pt,1]+(sgnY)*0.025, lab, fontsize=MEDIUM_SIZE)
   #axs.legend()
   #fig.legend(handles,     # The line objects
   #             labels,
@@ -290,7 +296,16 @@ if("SHOWPTS" in mode):
 else:  
   
   if(any(string in mode for string in ["PSD","THIS"])):
-    calcVar = 'P'
+    if("UU" in mode):
+      calcVar = 'U'
+    elif("VV" in mode):
+      calcVar = 'V'
+    elif("WW" in mode):
+      calcVar = 'W'
+    elif("PP" in mode):
+      calcVar = 'P'
+    else:
+      raise ValueError('ALYA ERROR :: CALCVAR NOT SPECIFIED!')
     print('--|| INFO :: PSD CALCULATIONS ON %s'% calcVar)
     print('--|| ALYA :: READING WITNESS DATA.')
     stime = time.time()
@@ -313,31 +328,34 @@ else:
     print('--|| ALYA :: ARRANGING ARRAYS MONOTONICALLY.')
     stime = time.time()
     tM = np.maximum.accumulate(time_data)
-    t, ind = np.unique(tM, return_index=True)
+    t, ind_unq = np.unique(tM, return_index=True)
     print('----|| INFO :: UNIQUE TIME ARRAY SIZE=',np.size(t))
     totalMarkPts = 1000;
     markFreq = int(np.amax((np.floor(len(t)/totalMarkPts),1),axis=None))
     print('--|| ALYA :: DONE. TIME=',time.time()-stime)
+    fig, axs = plt.subplots(len(indP), 2, sharex=True, figsize=(11.69,8.27))
+    #ax = plt.subplot(len(indP), 1, i+1)
+    ax = plt.gca()
     for (i,j) in enumerate(indP):
       print('----|| INFO :: PROCESSING POINT %d, x,y,z='%j, witPoints[j,:])
       #lab = r'$P_{'+str(j+1)+'}$';
       #lab = r'$x/c=%.3f, \, y/c=%.2f$'%(witPoints[j,0],witPoints[j,1]);
-      lab = r'$x/c=%.3f$'%(witPoints[j,0]);
+      lab = r'$x/c=%.1f$'%(witPoints[j,0]);
       if('ZAVG' in mode):
         print('----|| INFO :: PERFORMING SPANWISE AVERAGING')
         z_per_index = np.arange(j,nWit,nz)
+        #for idx in z_per_index:
+          #print('----|| X=%.2f, Y=%.3f, Z=%.3f '% (witPoints[idx,0],witPoints[idx,1],witPoints[idx,2]));
         dataPoint = np.mean(wit_data[:,z_per_index],axis=1)
       else:
         dataPoint = wit_data[:,j]; #Extract data
       print('----|| INFO :: WITNESS ARRAY SIZE=',np.shape(dataPoint))
       backFlow = 100.0*float(len(np.where(dataPoint<0.0)[0]))/len(dataPoint)
       print('----|| INFO :: PERCENT BACKFLOW=', backFlow)
-      dataPoint = dataPoint[ind];
+      dataPoint = dataPoint[ind_unq];
       print('----|| INFO :: UNIQUE WITNESS ARRAY SIZE=',np.shape(dataPoint))
       tSignal = t;
       ySignal = dataPoint-np.mean(dataPoint,axis=None)
-      #ax = plt.subplot(len(indP), 1, i+1)
-      ax = plt.gca()
       if("THIS" in mode):
         print('--|| ALYA :: CALCULATE TIME HISTORY (THIS).')
         N = 1000;
@@ -351,14 +369,6 @@ else:
          plt.xlabel(r'$tU_o/c$');
          ylab = r'$%s/U_o$'% calcVar.lower()
          plt.ylabel(ylab);
-        ##---subplot 2------#
-        #ax = plt.subplot(212)
-        #plt.plot(tSignal, ySignal, '.', markersize=5)
-        #plt.plot(tSignal, 0.0*dataPoint, 'k--', linewidth=1.0)
-        #if(i==len(indP)-1):
-        # plt.xlabel(r'$tU_o/c$');
-        # ylab = r'$%s''/U_o$'% calcVar.lower()
-        # plt.ylabel(ylab);
         plt.tight_layout()
         plt.suptitle(lab)
       elif("PSD" in mode):
@@ -372,7 +382,7 @@ else:
           delta_t = np.amax(np.diff(tSignal),axis=None)
           s_rate = 1.0/delta_t
           print('----|| INFO :: SAMPLING FREQ = %.2f'% s_rate)
-          if('INTERPFFT' in mode):
+          if('INTERP' in mode):
             Nt = int((np.amax(tSignal,axis=None)-np.amin(tSignal,axis=None))/delta_t)+1
             print('----|| INFO :: INTERPOLATING ON %d POINTS'%(Nt))
             print('--|| ALYA :: INTERPOLATE CONSTANT TIME.')
@@ -380,7 +390,7 @@ else:
             tSignal = np.linspace(np.amin(tSignal,axis=None),np.amax(tSignal,axis=None),Nt);
             ySignal = fInterp(tSignal)
             print('--|| ALYA :: DONE. TIME=',time.time()-stime)
-          hann_size = 2    
+          hann_size = 1    
           amp,pgram = calcSpectra(ySignal, hann_size, 0)
           #pgram = np.fft.fft(ySignal)
           N = len(pgram); print('--|| INFO :: FFT LENGTH = %d'%N) 
@@ -393,84 +403,117 @@ else:
           #ax.set(xlim=(0, 10))
         else:
           raise ValueError('ALYA ERROR :: METHOD TO CALC PSD NOT SPECIFIED!')
-        pgram = 100**i*(pgram/np.amax(pgram,axis=None))
-        plt.plot(f, pgram, markevery=markFreq,linewidth=0.5,label=lab)
+        #---Subplot 1----#
+        ax1 = axs[i,0]; ax2 = axs[i,1];
+        ax1.plot(f, pgram, markevery=markFreq,linewidth=1.0,label=lab)
         if('U' in calcVar):
-          plt.ylabel(r'$E_{u^\prime u^\prime}$')
+          ax1.set_ylabel(r'$E_{u^\prime u^\prime}$')
         elif('V' in calcVar):
-          plt.ylabel(r'$E_{v^\prime v^\prime}$')
+          ax1.set_ylabel(r'$E_{v^\prime v^\prime}$')
         elif('W' in calcVar):
-          plt.ylabel(r'$E_{w^\prime w^\prime}$')
+          ax1.set_ylabel(r'$E_{w^\prime w^\prime}$')
         elif('P' in calcVar):
-          plt.ylabel(r'$E_{p^\prime p^\prime}$')
-        plt.yscale("log")
-        plt.xscale("log")
-        ax.set(xlim=(np.amin(f), 1000))
+          ax1.set_ylabel(r'$E_{p^\prime p^\prime}$')
+        ax1.set_yscale("log")
+        ax1.set_xscale("log")
+        ax1.set_xlim(0.01, 100)
+        ax1.set_ylim(bottom=1e-12)
+        #ax1.legend(loc="lower left",fontsize='small',ncol=2)
+        #---Subplot 2----#
+        ax2.plot(f, np.multiply(f,pgram), markevery=markFreq,linewidth=1.0,label=lab)
+        if('U' in calcVar):
+          ax2.set_ylabel(r'$f E_{u^\prime u^\prime}$')
+        elif('V' in calcVar):
+          ax2.set_ylabel(r'$f E_{v^\prime v^\prime}$')
+        elif('W' in calcVar):
+          ax2.set_ylabel(r'$f E_{w^\prime w^\prime}$')
+        elif('P' in calcVar):
+          ax2.set_ylabel(r'$f E_{p^\prime p^\prime}$')
+        #plt.yscale("log")
+        ax2.set_xscale("log")
+        ax2.set_xlim(0.01, 100)
         #plt.gca().set_xlim(right=1000);
         #ax.annotate(lab, xy=(0.05, 1.1), xycoords='axes fraction', fontsize=MEDIUM_SIZE,
         #                horizontalalignment='left', verticalalignment='top')
-        ax.legend(loc="lower left")
+        #ax.legend(loc="lower left")
         #fig.legend(handles,     # The line objects
         #             labels,
         #             loc="upper right",   # Position of legend	i
         #             bbox_to_anchor=(0.91,0.52),
         #             ncol = 2, fancybox=True,shadow=True
         #            )
-        if(i==0):
-         plt.plot(f[np.where(f>100)],10.0*np.power(f[np.where(f>100)],-5.0/3),'k--')
-        elif(i==len(indP)-1):
-         plt.xlabel(r'$fc/U_o$');
+        ax1.plot(f[np.where(f>100)],np.power(f[np.where(f>100)],-5.0/3),'k--')
+        if(i==len(indP)):
+         ax1.set_xlabel(r'$fc/U_o$');
+         ax2.set_xlabel(r'$fc/U_o$');
+        #ax1.ticklabel_format(axis='both', style='sci')
+        ax2.ticklabel_format(axis='y', style='sci', scilimits=(0,0))
+        ## Hide x labels and tick labels for top plots and y ticks for right plots.
+        #for ax in axs.flat:
+        #  ax.label_outer()
       else:
         raise ValueError('ALYA ERROR :: CHECK MODE THAT YOU SPECIFIED!')
 
-      #--------TWEAK THE SUBPLOTS-----------#
-      if(i==0):
-       # Hide the right and top spines
-       ax.spines['top'].set_visible(False)
-       ax.spines['bottom'].set_visible(True)
-       ax.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=True,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=True) # labels along the bottom edge are off
-      elif(i==len(indP)-1):
-       ax.spines['top'].set_visible(False)
-       ax.spines['bottom'].set_visible(True)
-       # Only show ticks on the left and bottom spines
-       ax.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=True,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=True) # labels along the bottom edge are off
-      else:
-       ax.spines['bottom'].set_visible(False)
-       ax.spines['top'].set_visible(False)
-       ax.tick_params(
-        axis='x',          # changes apply to the x-axis
-        which='both',      # both major and minor ticks are affected
-        bottom=False,      # ticks along the bottom edge are off
-        top=False,         # ticks along the top edge are off
-        labelbottom=False) # labels along the bottom edge are off
-       
-      ax.spines['right'].set_visible(False)
-      ax.tick_params(
-        axis='y',          # changes apply to the x-axis
-        which='minor',      # both major and minor ticks are affected
-        left=False,      # ticks along the bottom edge are off
-        right=False)         # ticks along the top edge are off
-      ax.tick_params(
-        axis='y',          # changes apply to the x-axis
-	which='major',      # both major and minor ticks are affected
-	right=False)         # ticks along the top edge are off
+      ##--------TWEAK THE SUBPLOTS-----------#
+      #if(i==0):
+      # # Hide the right and top spines
+      # ax.spines['top'].set_visible(False)
+      # ax.spines['bottom'].set_visible(True)
+      # ax.tick_params(
+      #  axis='x',          # changes apply to the x-axis
+      #  which='both',      # both major and minor ticks are affected
+      #  bottom=True,      # ticks along the bottom edge are off
+      #  top=False,         # ticks along the top edge are off
+      #  labelbottom=True) # labels along the bottom edge are off
+      #elif(i==len(indP)-1):
+      # ax.spines['top'].set_visible(False)
+      # ax.spines['bottom'].set_visible(True)
+      # # Only show ticks on the left and bottom spines
+      # ax.tick_params(
+      #  axis='x',          # changes apply to the x-axis
+      #  which='both',      # both major and minor ticks are affected
+      #  bottom=True,      # ticks along the bottom edge are off
+      #  top=False,         # ticks along the top edge are off
+      #  labelbottom=True) # labels along the bottom edge are off
+      #else:
+      # ax.spines['bottom'].set_visible(False)
+      # ax.spines['top'].set_visible(False)
+      # ax.tick_params(
+      #  axis='x',          # changes apply to the x-axis
+      #  which='both',      # both major and minor ticks are affected
+      #  bottom=False,      # ticks along the bottom edge are off
+      #  top=False,         # ticks along the top edge are off
+      #  labelbottom=False) # labels along the bottom edge are off
+      # 
+      #ax.spines['right'].set_visible(False)
+      #ax.tick_params(
+      #  axis='y',          # changes apply to the x-axis
+      #  which='minor',      # both major and minor ticks are affected
+      #  left=False,      # ticks along the bottom edge are off
+      #  right=False)         # ticks along the top edge are off
+      #ax.tick_params(
+      #  axis='y',          # changes apply to the x-axis
+      #  which='major',      # both major and minor ticks are affected
+      #  right=False)         # ticks along the top edge are off
 
-      # plt.locator_params(axis='y', nbins=3)
+      ## plt.locator_params(axis='y', nbins=3)
     print('--|| ALYA :: DONE. TIME=',time.time()-stime)
 
      
   elif("TPCORR" in mode):
-    calcVar = 'UU'
+    if("UU" in mode):
+      calcVar = 'UU'
+    elif("VV" in mode):
+      calcVar = 'VV'
+    elif("WW" in mode):
+      calcVar = 'WW'
+    elif("PP" in mode):
+      calcVar = 'PP'
+    elif("UV" in mode):
+      calcVar = 'UV'
+    else:
+      raise ValueError('ALYA ERROR :: TPC VARIABLE NOT SPECIFIED!')
+      
     print('--|| INFO :: TWO-POINT CORRELATION ON %s'% calcVar)
     print('--|| ALYA :: READING WITNESS DATA.')
     stime = time.time()
@@ -492,6 +535,7 @@ else:
     else:
       raise ValueError('ALYA ERROR :: VARIABLE NOT SPECIFIED!')
     print('----|| INFO :: %d TIME VALUES RANGE %.2f-%.2f'%(len(time_data),time_data[0], time_data[-1]))
+    print('----|| INFO :: LOADED %d COLUMNS'%(len(wit_data_0[0,:])))
 
     print('--|| ALYA :: DONE. TIME=',time.time()-stime)
     markFreq = int(np.amax((np.floor(len(z)/20),1),axis=None))
@@ -499,13 +543,21 @@ else:
     print('--|| ALYA :: CALCULATING WIT POINT CORRELATIONS.')
     stime = time.time()
     uu = np.zeros((len(z),len(indP)),dtype=float)
-    for j in indPLabel:
-     u0 = wit_data_0[:,j+1]; u0 = u0-np.mean(u0);
+    for j in range(len(indP)):
+     pair1 = indP[j]
+     u0 = wit_data_0[:,pair1]; u0 = u0-np.mean(u0);
+     if('RAVG' in mode):
+       #Ns = int(np.ceil(float(len(time_data))/10));
+       Ns = 2000;
+       u0 = u0 - np.convolve(u0, np.ones(Ns)/Ns, mode='same')
      for i in range(len(z)):
-      pair = i*nz+j+1;
-      #print(j+1,pair);
+      pair2 = i*nz+pair1;
+      # print(pair1,pair2);
+      # print('----|| X=%.2f, Y=%.3f, Z=%.3f '% (witPoints[pair2,0],witPoints[pair2,1],witPoints[pair2,2]));
       # Calculate the correlation b/w 2 signals
-      u1 = wit_data_1[:,pair]; u1 = u1-np.mean(u1);
+      u1 = wit_data_1[:,pair2]; u1 = u1-np.mean(u1);
+      if('RAVG' in mode):
+        u1 = u1 - np.convolve(u1, np.ones(Ns)/Ns, mode='same')
       prod = np.multiply(u0,u1)
       uu[i,j] = np.mean(prod)/(np.sqrt(np.mean(u0*u0))*np.sqrt(np.mean(u1*u1)))
     print('--|| ALYA :: DONE. TIME=',time.time()-stime)
@@ -516,17 +568,26 @@ else:
     grid = plt.GridSpec(1, 1, wspace=0.3, hspace=0.8)
     
     axs = plt.subplot(grid[0, 0:]);
-    for i in indPLabel:
-      lab = r"$P_{"+str(indP[i]+1)+"}$";
+    for i in range(len(indP)):
+      lab = r"$P_{"+str(i+1)+"}$";
       print("--|| ALYA :: PROCESSING POINT",lab);
+      print('----|| X=%.2f, Y=%.3f'% (witPoints[i,0],witPoints[i,1]))
       lname, lcode = linestyles[j][0:]
-      l1, = plt.plot(z, uu[:,i], ls[i], linewidth=lw, color='black', 
+      if('POLY' in mode):
+        order = 15
+        uu_avg = uu[:,i]
+        uu_avg = np.polyval(np.polyfit(z,uu_avg,order),z)
+      else:
+        uu_avg = uu[:,i]
+      l1, = plt.plot(z, uu_avg, ls[i], linewidth=lw, color='black',markersize=4,
             markevery=markFreq, markerfacecolor='black', markeredgecolor='black', label=lab)
+    plt.axhline(y=0.0, color='g', linestyle='-', linewidth=0.5)
     plt.xlabel(r'$z/c$');
-    plt.ylabel(r'$\mathcal{R}_{u^\prime u^\prime}$')
+    labStr = r'$\mathcal{R}_{\langle %s^\prime %s^\prime\rangle}$'%(mode[7].lower(),mode[8].lower())
+    plt.ylabel(labStr)
     plt.xticks([0,0.05,0.1])
     #plt.yticks([0,0.5,1])
-    axs.set(xlim=(0, 0.1))
+    axs.set(xlim=(0,np.amax(z)))
     
     plt.tick_params(
         axis='y',          # changes apply to the x-axis
@@ -536,8 +597,8 @@ else:
         labelleft=True,
         labelright=False) # labels along the bottom edge are off
     axs.legend(loc="upper right",   # Position of legend
-               bbox_to_anchor=(0.92,0.92),
-               ncol = 2, fancybox=True
+               bbox_to_anchor=(0.98,0.98),
+               ncol = 2, frameon=True, edgecolor='black'
               )
   elif("BACKFLOW" in mode):
     import pandas as pd
@@ -733,8 +794,8 @@ elif('THIS' in mode):
   listStr = "_".join(listP)
   savePath = savePath+listStr+'_'
   savePath = savePath+calcVar.lower()+'_'
-elif('TPCORR' in mode):
-  savePath = savePath+calcVar.lower()+'_'
+#elif('TPCORR' in mode):
+#  savePath = savePath+'_'
 elif('BACKFLOW' in mode):
   listP = [str(pt) for pt in indP]
   listStr = "_".join(listP)
