d76ce38fd334e4d63f42cdda42cbe058152269b1
Vishal Kumar <kumarv@polaris-login-01.hsn.cm.polaris.alcf.anl.gov>
2023-06-19 13:26:06 +0000

    script to track SOD2D sim

---
+++ sod_sim_hist.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('analysis_channel-1.dat', skiprows=0)
+
+time    = data[:,0];
+Ek      = data[:,1];
+eps_S   = data[:,2];
+eps_D   = data[:,3];
+eps_T   = data[:,4];
+Mmax    = data[:,5];
+ylab = [r'$E_k$',r'$eps_{S}$',r'$eps_{D}$',r'$eps_{T}$',r'$M_{max}$','']
+
+fig, axs = plt.subplots(3, 2, sharex=True, dpi=300)
+
+axs[0,0].plot(time,Ek,linewidth=1.5,label='Ek')
+axs[0,1].plot(time,eps_S,linewidth=1.5,label=r'$eps_S$')
+axs[1,0].plot(time,eps_D,linewidth=1.5,label=r'$eps_D$')
+axs[1,1].plot(time,eps_T,linewidth=1.5,label=r'$eps_T$')
+axs[2,0].plot(time,Mmax,linewidth=1.5,label=r'$M_{max}$')
+
+plt.xlabel(r'$t$')
+
+for i,ax in enumerate(axs.flat):
+    ax.set(ylabel=ylab[i])
+    if(i in [4,5]):
+      ax.set(xlabel=r'$t$')
+plt.tight_layout()
+plt.savefig('sod_analysis.png')
+#plt.show()
